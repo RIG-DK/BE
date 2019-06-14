7944aee06b608d9f07475edc52c6c30d11d714a4
yannafaith <yannafaith97@gmail.com>
2019-06-14 05:05:08 -0700

    makes titles for posts unique

--- database/migrations/20190613042549_posts_table.js
+++ database/migrations/20190613042549_posts_table.js
@@ -1,7 +1,7 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('posts', table => {
         table.increments();
-        table.string('title').notNullable();
+        table.string('title').notNullable().unique();
         table.text('body').notNullable();
         table.text('summary')
         table.integer('reactions');
